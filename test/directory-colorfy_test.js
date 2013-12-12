bb25a87de8298326b1969eaae77e71b072d7fde9
Jeffrey Lembeck <jlembeck@gmail.com>
2013-12-12 14:41:47 -0800

    Add directory-colorfy stuff

--- test/directory-colorfy_test.js
+++ test/directory-colorfy_test.js
@@ -1,36 +1,63 @@
-'use strict';
 
-var directory_colorfy = require('../lib/directory-colorfy.js');
+/*global require:true*/
+(function( exports ){
+	"use strict";
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+	var path = require( 'path' );
+	var fs = require( 'fs' );
+	var DirectoryColorfy = require( path.join( '..', 'lib', 'directory-colorfy' ) );
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(directory_colorfy.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
-};
+	exports.constructor = {
+		setUp: function( done ) {
+			this.dc = new DirectoryColorfy( "test/files" , "test/files" );
+			this.dc2 = new DirectoryColorfy( "test/files", "test/files/temp",
+																			{ colors: {
+																					"blue": "blue"
+																				}
+																			});
+			done();
+		},
+		tearDown: function( done ){
+			done();
+		},
+		emptyConstructor: function( test ){
+			test.equal( Object.keys( this.dc.options ).length, 0 , "Empty options hash on constructor" );
+			test.done();
+		},
+		constructor: function( test ){
+			test.equal( this.dc2.input, "test/files", "Input filled in on constuctor" );
+			test.equal( this.dc2.output, "test/files/temp", "Output filled in on constuctor" );
+			test.equal( Object.keys( this.dc2.options.colors ).length, 1, "Colors filled" );
+			test.equal( Object.keys(this.dc2.options.colors)[0], "blue", "Colors filled" );
+			test.done();
+		}
+	};
+	exports.convert = {
+		setUp: function( done ) {
+			this.dc = new DirectoryColorfy( path.resolve( path.join( "test", "files", "directory-colorfy" )), path.resolve( path.join( "test", "files", "temp" )),
+																		{ colors: {
+																				"green": "green",
+																				"orange": "orange"
+																			}
+																		});
+			done();
+		},
+		tearDown: function( done ){
+			["bear", "bear-green", "bear-orange"].forEach( function( base ){
+				if( fs.existsSync( "test/files/temp/" + base + ".svg" ) ){
+					fs.unlinkSync( "test/files/temp/" + base + ".svg" );
+				}
+			});
+			done();
+		},
+		convert: function( test ){
+			this.dc.convert();
+			test.ok( fs.existsSync( "test/files/temp/bear-green.svg" ) , "Green bear is there" );
+			test.ok( fs.existsSync( "test/files/temp/bear-orange.svg" ) , "Orange bear is there" );
+			test.done();
+		}
+	};
+
+}(typeof exports === 'object' && exports || this));
+
