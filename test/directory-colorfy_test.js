84b41c21b95caed81929e6e46012347d27864913
Jeffrey Lembeck <jlembeck@gmail.com>
2013-12-13 10:24:18 -0800

    Directory-colorfy no longer needs a dir as an input, can also take an array of files

--- test/directory-colorfy_test.js
+++ test/directory-colorfy_test.js
@@ -16,6 +16,7 @@
 																					"blue": "blue"
 																				}
 																			});
+			this.dc3 = new DirectoryColorfy( ["test/files/bear.colors-blue-red.svg"] , "test/files" );
 			done();
 		},
 		tearDown: function( done ){
@@ -26,10 +27,12 @@
 			test.done();
 		},
 		constructor: function( test ){
+			test.equal( this.dc.files.length , 4, "Amount of files correct" );
 			test.equal( this.dc2.input, "test/files", "Input filled in on constuctor" );
 			test.equal( this.dc2.output, "test/files/temp", "Output filled in on constuctor" );
 			test.equal( Object.keys( this.dc2.options.colors ).length, 1, "Colors filled" );
 			test.equal( Object.keys(this.dc2.options.colors)[0], "blue", "Colors filled" );
+			test.equal( this.dc3.files.length , 1, "Amount of files correct" );
 			test.done();
 		}
 	};
@@ -41,10 +44,16 @@
 																				"orange": "orange"
 																			}
 																		});
+			this.dc2 = new DirectoryColorfy( ["test/files/directory-colorfy/cat.svg"] , path.resolve( path.join( "test", "files", "temp" )),
+																		{ colors: {
+																				"green": "green",
+																				"orange": "orange"
+																			}
+																		});
 			done();
 		},
 		tearDown: function( done ){
-			["bear", "bear-green", "bear-orange"].forEach( function( base ){
+			["bear", "bear-green", "bear-orange", "cat", "cat-green", "cat-orange"].forEach( function( base ){
 				if( fs.existsSync( "test/files/temp/" + base + ".svg" ) ){
 					fs.unlinkSync( "test/files/temp/" + base + ".svg" );
 				}
@@ -53,8 +62,11 @@
 		},
 		convert: function( test ){
 			this.dc.convert();
+			this.dc2.convert();
 			test.ok( fs.existsSync( "test/files/temp/bear-green.svg" ) , "Green bear is there" );
 			test.ok( fs.existsSync( "test/files/temp/bear-orange.svg" ) , "Orange bear is there" );
+			test.ok( fs.existsSync( "test/files/temp/cat-green.svg" ) , "Green cat is there" );
+			test.ok( fs.existsSync( "test/files/temp/cat-orange.svg" ) , "Orange cat is there" );
 			test.done();
 		}
 	};
